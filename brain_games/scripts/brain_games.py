19a9d5a5b31b977b23fca37f8449aaa48fa8192c
Sasha Koch <pollutana@proton.me>
2023-12-16 17:36:41 +0000

    feat: Implement user input with the prompt package.

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+import prompt
 
 
 def main():
     print("Welocme to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
 
 
 if __name__ == "__main__":
