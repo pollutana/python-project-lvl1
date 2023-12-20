9b4d19c66c164d1756003991bfd1f0efda3c9854
Sasha Koch <pollutana@proton.me>
2023-12-20 11:48:30 +0000

    feat: arithmetic progression game and its corresponding script

---
+++ brain_games/games/progression.py
@@ -0,0 +1,27 @@
+import random
+
+RULE = "What number is missing in the progression?"
+_PROG_MIN = 5
+_PROG_MAX = 10
+_HIDE_SYMBOL = ".."
+
+
+def _generate_progression() -> list:
+    prog_len = random.randint(_PROG_MIN, _PROG_MAX + 1)
+    step = random.randint(1, 11)
+    start = random.randint(0, 100)
+    stop = start + step * prog_len
+    return list(range(start, stop, step))
+
+
+def check_answer(a, b):
+    return a == b
+
+
+def generate_question_and_answer() -> tuple:
+    prog = _generate_progression()
+    hidden_index = random.randint(0, len(prog) - 1)
+    correct_answer = str(prog[hidden_index])
+    prog[hidden_index] = _HIDE_SYMBOL
+    question_text = "Question: " + " ".join(map(str, prog))
+    return question_text, correct_answer
